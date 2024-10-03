093a7144ec5f39c1b1443372b1bc404e806d4b0c
SandmanMaybeTe23 <billt.burlin@elev.ga.ntig.se>
2024-10-03 09:46:03 +0200

    inproving and fixing

--- brawl.py
+++ brawl.py
@@ -11,13 +11,14 @@ doge1=False
 doge2=False
 healed1=0
 healed2=0
+doge1chance=0
+doge2chance=0
 player_name1=input("hello player one what is your name")
-player_or_computer=input(f"{player_name1} do you wan another player to fight Y/N")
+player_or_computer=input(f"{player_name1} do you wan another player to fight Y/N").upper()
 player2=False
 player_class2=False
 game_round=0
-game="off"
-player_or_computer_converter = str.upper(player_or_computer)
+game=0
 if player_or_computer == "Y":
     print("Good")
     player_name2=input("So what is player two name?")
@@ -34,17 +35,16 @@ elif player_or_computer != "Y" or "N":
 
 
 player_class1=input(f"{player_name1} what class do you can pick from /S/ for solder and solder give you extra lives /M/ for medic  you  have less live but a chance to get live per round or /B/ for baller that you get less live but you can doge attacks").upper() 
-print(f"{player_class1}")
 
 if player_class1 == "S":
-    player1_lives=30
+    player1_lives=15
     print(f"{player_name1} you have pick solder you have 20 lives")
 elif player_class1 == "M":
-    player1_lives=10
+    player1_lives=5
     healing1=True
     print(f"{player_name1} you have pick MEDIC you have 10 live and you can heal your self ")
 elif player_class1 == "B":
-    player1_lives=15
+    player1_lives=10
     doge1=True
     print(f"{player_name1} you have pick baller and you have 15 lives")
 
@@ -53,42 +53,47 @@ if player2 == True :
     player_class2=input(f"{player_name2} what class do you want /S/ for Solder /M/ for Medic /B/ for Baller").upper()
 
 if player_class2 == "S":
-    player2_lives=30
+    player2_lives=15
     print(f"{player_name2} you have pick solder you have 20 lives")
     game="on"
 elif player_class2 == "M":
-    player2_lives=10
+    player2_lives=5
     healing2=True
     print(f"{player_name2} you have pick MEDIC you have 10 live and you can heal your self ")
     game="on"
 elif player_class2 == "B":
-    player2_lives=15
+    player2_lives=10
     doge2=True
     print(f"{player_name2} you have pick baller and you have 15 lives")
-    game="no"
+    game="on"
 
-if player2 == False :
+if player2==False:
     ai_class=randint(1,3)
+    
     if ai_class==1:
-        player2_lives=20
+        player2_lives=15
         print(f"{player_name2} has picket solder")
         game="on"
     elif ai_class == 2:
-        player_class2=10
+        player_class2=5
         healing2=True
         print(f"{player_name2} has picket Medic")
         game="on"
     elif ai_class==3:
-        player2_lives=15
+        player2_lives=10
         doge2=True
         print(f"{player_name2} hasa picket baller")
         game="on"
     
-    
+player1_lost = False 
+player2_lost = False
 while game == "on":
-    game_round=+1
+    game_round+=1
     player1_lost=False
     player2_lost=False
+    print(f"{player_name1} has {player1_lives}")
+    print(f"{player_name2} has {player2_lives}")
+    print(f"the Round{game_round}")
     print("Round staring in 3")
     time.sleep(1)
     print("2")
@@ -98,93 +103,88 @@ while game == "on":
     print(f"{player_name1} rolls")
     player1_roll=randint(1,6)
     print("4")
-    time.sleep(0.5)
+    time.sleep(0.2)
     print("6")
-    time.sleep(0.5)
-    print("2")
-    time.sleep(0.5)
-    print("1")
-    time.sleep(0.5)
-    print("5")
-    time.sleep(0.5)
+    time.sleep(0.2)
+    print("3")
+    time.sleep(0.2)
     print(f"{player1_roll}")
     time.sleep(1)
     print(f"{player_name2} is now rolling")
     player2_roll=randint(1,6)
     print("3")
-    time.sleep(0.5)
-    print("1")
-    time.sleep(0.5)
-    print("4")
-    time.sleep(0.5)
+    time.sleep(0.2)
     print("5")
-    time.sleep(0.5)
+    time.sleep(0.2)
     print("6")
-    time.sleep(0.5)
+    time.sleep(0.2)
     print(f"{player2_roll}")
-
+    
+    
     if player1_roll>player2_roll:
-        player2_lives=-1
+        player2_lives-=1
+        print(f"{player_name1} has won")
         player2_lost=True
-        print(f"{player_name1} has won {game_round}")
-    elif player1_roll<player2_roll:
-        player1_lives =-1
+    elif player2_roll>player1_roll:
+        player1_lives-=1
+        print(f"{player_name2} has won")
         player1_lost=True
-        print(f"{player_name2} has won {game_round}")
     elif player1_roll==player2_roll:
-        print("NO one won :l")
+        print("NO ONE WON :L")
 
     
-    
-    if healing1==True:
-        healed1=randint(1,3)
-   
-    if  healed1 ==1:
-        print(f"{player_name1} has healed 0 lives")
-    elif healed1 ==2:
-        player1_lives=+1
-        print(f"{player_name1} has healed 1 lives")
-    elif healed1 ==3:
-        player1_lives =+2
-        print(f"{player_name1} has healed 2 lives")
-   
-   
-    if healing2==True:
+    if healing2 == True and player2_lost == True:
         healed2=randint(1,3)
     
-    if healed2 ==1:
-        print(f"{player_name2} has healed 0") 
-    elif healed2 ==2:
-        player1_lives=+1
-        print(f"{player_name2} has healed 1")
-    elif healed2 ==3:
-        player2_lives =+2
-        print(f"{player_name2} has healed 2")
-    
-    if player1_lost==True: 
-        doge1chance=randint(1,5)
+    if healing1 == True and player1_lost == True:
+        healed1=randint(1,3)
 
-    if doge1==True:
-        doge1_success=doge1chance
+    if doge2 == True and player2_lost == True:
+        doge2chance=randint(1,5)
+   
+    if doge1 == True and player1_lost == True:
+        doge1chance=randint(1,5)
     
-    if doge1_success== 4 or 5 :
-        player1_lives=+1
-        print(f"{player_name1} doge the attack")
-    elif doge1_success == 1 or 2 or 3 :
-        print(f"{player_name1} did not doge")
+   
+    if healed1 ==3 and player1_lost == True:
+        player1_lives+=2 
+    else:
+       print(f"{player_name1} no heals")
+    
+    if healed2 == 3 and player2_lost == True:
+        player2_lives+=2 
+    else:
+        print(f"{player_name2} no heals")
+     
+     
+
+   
+    if doge1chance == 4 or 5 and  player1_lost == True :
+        print(f"{player_name1}yes doge")
+        player1_lives+=1
+    elif doge1chance == 1 or 2 or 3 and player1_lost == True:
+        print(f"{player_name1}no doge")
+    
+    if doge2chance == 4 or 5 and player2_lost == True:
+        print(f"{player_name2}yes doge")
+        player2_lives+=1
+    elif doge2chance == 1 or 2 or 3 and player2_lost == True:
+        print(f"{player_name2}no doge")
+
+
+
+
+
+
+    
+    
+
+   
+   
+
 
         
-    if player2_lost==True: 
-        doge2chance=randint(1,5)
-
-    if doge1==True:
-        doge2_success=doge2chance
-    
-    if doge2_success== 4 or 5 :
-        player2_lives=+1
-        print(f"{player_name2} doge the attack")
-    elif doge2_success == 1 or 2 or 3 :
-        print(f"{player_name2} did not doge")
+   
         
 
     
@@ -195,3 +195,5 @@ while game == "on":
     elif player2_lives==0:
         print(f"{player_name1} won the game")
         game = "off" 
+    elif game_round == 10:
+        game = "off"
